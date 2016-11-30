bfbfdc0f01b308ce3ca17c3ef329f91d4f054346
Juraj Oršulić <juraj5@gmail.com>
2016-11-30 15:23:29 +0100

    do_filtering.m cleanup

--- util/do_filtering.m
+++ util/do_filtering.m
@@ -1,42 +1,52 @@
 %% perform filtering with median and Holt-Winters
-median_window_size = 1500;
+active_settings = 1;  % 1 - imu, 2 - camera
 
-sensor_framerate = 200;  % for adjusting X axis to seconds
+clock_readings = numbers_imu(:,2) - numbers_imu(1,2);
+sensor_readings = numbers_imu(:,3) - numbers_imu(1,3);
+current_hyp_t0 = clock_readings - sensor_readings;
 
-alfa = 0.0004;
-beta = 0.00004;
+switch active_settings
+  case 1 % imu
+    median_window_size = 1500;
 
-clamp_time = 120*200;
-early_alfa_start = 0.05;
-early_beta_start = 0.001;
+    sensor_framerate = 200;
 
-use_med_filtering = true;
-initial_b = -3.4e-7;
+    alfa = 0.0004;
+    beta = 0.0003;
 
+    clamp_time = 120*200;
+    early_alfa_start = 0.05;
+    early_beta_start = 0.001;
 
-% median_window_size = 5000;
-% 
-% sensor_framerate = 20;  % for adjusting X axis to seconds
-% 
-% alfa = 0.001;
-% beta = 0.0001;
-% 
-% clamp_time = 500;
-% early_alfa_start = 0.1;
-% early_beta_start = 0.0;
-% 
-% use_med_filtering = true;
-% initial_b = -3e-7;
+    use_med_filtering = true;
+    initial_b = -3.4e-7;
+    
+  case 2 % camera
+    median_window_size = 2500;
+
+    sensor_framerate = 20;
+
+    alfa = 0.003;
+    beta = 0.002;
+
+    clamp_time = 500;
+    early_alfa_start = 0.1;
+    early_beta_start = 0.0;
+
+    use_med_filtering = true;
+    initial_b = -3e-7;
+end
+
 %%%%
 
-n = size(numbers, 1);
+n = size(current_hyp_t0, 1);
 s = zeros(n, 1);
 b = s;
 
 if use_med_filtering
-    med_filtered = medfilt(median_window_size, numbers(:, 6));
+    med_filtered = medfilt(median_window_size, current_hyp_t0);
 else
-    med_filtered = numbers(:, 6); % skip filtering
+    med_filtered = current_hyp_t0; % skip filtering
 end
 
 % the median filter needs ~500 samples to stabilize itself, we clamp to it
@@ -57,12 +67,11 @@ for i=2:n
       b(i)=beta*(s(i)-s(i-1))+(1-beta)*b(i-1);
     end
 end
-%s(1:clamp_time) = med_filtered(1:clamp_time);
 
 x = (1:n)/sensor_framerate;
 figure(1);
 hold off;
-plot(x, numbers(:,6));
+plot(x, current_hyp_t0);
 hold on; 
 if use_med_filtering
     plot(x, med_filtered);
@@ -75,5 +84,5 @@ plot(x(clamp_time:end), b(clamp_time:end));
 title('estimated drift speed')
 figure(3);
 hold off;
-plot(x(clamp_time:end), numbers(clamp_time:end,6)- s(clamp_time:end))
-title('error (current frame filtered timestamp - current clock)')
+plot(x(clamp_time:end), current_hyp_t0(clamp_time:end)- s(clamp_time:end))
+title('error (current t0 hypothesis - smoothed t0 hypothesis)')
